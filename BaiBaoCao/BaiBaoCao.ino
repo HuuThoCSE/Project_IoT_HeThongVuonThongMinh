e776619a3424b57b4459cedd4014eb6deb64c2dc
Hữu Thọ CS <89997693+huuthocs@users.noreply.github.com>
2023-02-05 16:32:40 +0700

    4h32om5-2-2023

--- BaiBaoCao/BaiBaoCao.ino
+++ BaiBaoCao/BaiBaoCao.ino
@@ -1,7 +1,13 @@
 // Import thư viện
 #include <LiquidCrystal.h> // Khai báo thư viện LCD / Kết nối qua SPI (Được hỗ trợ sẵn)
 #include <DHT.h> // Thư viện DHT11
-#include <BlynkSimpleStream.h>
+#include <BlynkSimpleStream.h> // Blynk
+#include <Wire.h> // RTC
+#include <RTClib.h>
+
+// RTC
+RTC_DS1307 rtc;
+char daysOfTheWeek[7][12]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
 
 // Khởi tạo chân
 LiquidCrystal lcd(9, 8, 5, 4, 3, 2); // Khởi tạo chân giao tiếp
@@ -15,27 +21,36 @@ SoftwareSerial DebugSerial(10, 11); // RX, TX
 #define BLYNK_AUTH_TOKEN "1-OlCZbBn0qtYwpZYQI0fnF3av7OvxKJ"
 char auth[] = "1-OlCZbBn0qtYwpZYQI0fnF3av7OvxKJ";
 
-const int ldrPin = A0;
-const int DHTPIN = A1;  
-const int pinSOIL = A2;    
-const byte motorPin = 7;
-const byte LAMpPin = 6;
+// Khai báo chân (Hằng số)
+// #define DHTPIN A1
+#define pinSOIL A2
+#define ldrPin 13
+#define pinRAIN 12
+#define motorPin 7
+#define LAMpPin 6
 
-const int DHTTYPE = DHT11;  
+#define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-int lightAnalog = 0;
-int valueDoAmDat = 0;
-float giatriDoAm, t = 0;
+int statusLight, valueDoAmDat, statusRain = 0;
+float giatriDoAm, giatriNhietDo = 0;
+DateTime now;
 
 void setup() {
   Serial.begin(9600);
 
+  rtc.begin();
+  if (!rtc.isrunning()){
+    Serial.println("RTC is NOT running");
+    rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
+  }
+
   lcd.begin(16, 2); // Khởi tạo LCD 16, 2
-  dht.begin();
+  // dht.begin();
 
   pinMode(ldrPin, INPUT);
   pinMode(pinSOIL, INPUT);
+  pinMode(pinRAIN, INPUT);
   pinMode(motorPin, OUTPUT);
   pinMode(LAMpPin, OUTPUT);
 
@@ -43,38 +58,65 @@ void setup() {
   Blynk.begin(Serial, auth);
 }
 
-void DoNhietdoDoAm(){
-  giatriDoAm = dht.readHumidity(); 
-  // t = dht.readTemperature();
-  t = random(30, 50);
-  Blynk.virtualWrite(V1, t);
-  DebugSerial.print("Nhiet do: ");
-  DebugSerial.println(t);
+void ModuleRTC(){
+  // LAY THOI GIAN TU RTC
+  now=rtc.now();
+  DebugSerial.print(now.year(), DEC);
+  DebugSerial.print(" ");
+  DebugSerial.print(now.month(), DEC);
+  DebugSerial.print(" ");
+  DebugSerial.print(now.day(), DEC);
+  DebugSerial.print(" ");
+  DebugSerial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  DebugSerial.print(" ");
+  DebugSerial.print(now.hour(), DEC);
+  DebugSerial.print(" ");
+  DebugSerial.print(now.minute(), DEC);
+  DebugSerial.print(" ");
+  DebugSerial.println(now.second(), DEC);
 }
 
-void ChecklightAnalog(){
-  // lightAnalog = analogRead(ldrPin); //lấy giá trị độ sáng từ 0 đến 1023
-  // lightAnalog = map(lightAnalog, 0, 1023, 0, 100);
-  lightAnalog = random(100);
-  Blynk.virtualWrite(V0, lightAnalog);
+void DoNhietdoDoAm(){
+  giatriDoAm = dht.readHumidity(); 
+  giatriNhietDo = dht.readTemperature();
+  // giatriDoAm = random(100);
+  // t = random(30, 50);
+  Blynk.virtualWrite(V6, giatriDoAm);
+  Blynk.virtualWrite(V1, giatriNhietDo);
+  DebugSerial.print("Nhiet do: ");
+  DebugSerial.println(giatriNhietDo);
+}
+
+void sersorLight(){
+  statusLight = digitalRead(ldrPin); 
+  Blynk.virtualWrite(V0, statusLight);
   DebugSerial.print("Anh sang: ");
-  DebugSerial.println(lightAnalog);
+  DebugSerial.println(statusLight);
 }
 
 void DoDoAmDat(){
-  // valueDoAmDat = analogRead(pinSOIL); //lấy giá trị độ sáng từ 0 đến 1023
-  // valueDoAmDat = map(valueDoAmDat, 0, 1023, 0, 100);
-  valueDoAmDat = random(100);
+  valueDoAmDat = analogRead(pinSOIL); //lấy giá trị độ sáng từ 0 đến 1023
+  valueDoAmDat = map(valueDoAmDat, 0, 1023, 0, 100);
+  // valueDoAmDat = random(100);
   Blynk.virtualWrite(V2, valueDoAmDat);
   DebugSerial.print("Do am dat: ");
   DebugSerial.println(valueDoAmDat);
+}
+
+void sersorRain(){
+  statusRain = digitalRead(pinRAIN);
+  DebugSerial.print("Thoi tiet: ");
+  if (statusRain == 1)
+    DebugSerial.println("Mua.");
+  else
+    DebugSerial.println("Nang.");
 }
 
 void PrintLCD(){
   lcd.clear(); // Xóa dữ liệu LCD
   lcd.setCursor(0,0); // Đặt con trỏ ở vị trí cột 0, hàng 0
   lcd.print("T:");
-  lcd.print(t);
+  lcd.print(giatriNhietDo);
 
   lcd.setCursor(8, 0);
   lcd.print("SOIL:");
@@ -87,30 +129,38 @@ void PrintLCD(){
 
   lcd.setCursor(10, 1);
   lcd.print("AS:");
-  lcd.print(lightAnalog);
-
+  lcd.print(statusLight);
 }
 
 void loop() {
-  
+  Blynk.run();
+  ModuleRTC();
   DoNhietdoDoAm();
-  ChecklightAnalog();
+  sersorLight();
   DoDoAmDat();
+  sersorRain();
   PrintLCD();
 
-  if(valueDoAmDat <= 70){
-    digitalWrite(motorPin, HIGH);
-  } else {
+  //CAI DAT GIO TUOI CAY HANG NGAY (BUỔI SÁNG VÀ TỐI)
+  if(((now.hour()>=7 && now.hour()<8)&&(now.minute()>=30&&now.minute()<=50))
+                      ||
+    ((now.hour()>=17 && now.hour()<18)&&(now.minute()>=30&&now.minute()<=50))){
+    digitalWrite(motorPin,HIGH);
+  }
+  else
+    digitalWrite(motorPin,LOW);
+
+  if(valueDoAmDat <= 45)
+    if (statusRain == 0)
+      digitalWrite(motorPin, HIGH);
+    else 
+      digitalWrite(motorPin, LOW);
+  else 
     digitalWrite(motorPin, LOW);
-  }
-
-  if(lightAnalog < 50){
+  
+  if(statusLight == 0)
     digitalWrite(LAMpPin, HIGH);
-  } else {
+  else 
     digitalWrite(LAMpPin, LOW);
-  }
-
-  Blynk.run();
-  delay(200);
   DebugSerial.println();
 }
---
+++ Cay001/Cay001.ino
@@ -0,0 +1,59 @@
+#include <BlynkSimpleStream.h> // Blynk
+#include <DHT.h> // Thư viện DHT11
+
+#define pinLight 4
+#define DHTPIN A0
+#define pinSOIL A1
+
+#include <SoftwareSerial.h>
+SoftwareSerial DebugSerial(2, 3); // RX, TX
+
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define BLYNK_PRINT DebugSerial
+#define BLYNK_TEMPLATE_ID "TMPLSWy0uUgW"
+#define BLYNK_DEVICE_NAME "CayTrai"
+#define BLYNK_AUTH_TOKEN "Tts2WJWwPD_a1c0OejzikcYrdszZwrKk"
+char auth[] = "Tts2WJWwPD_a1c0OejzikcYrdszZwrKk";
+
+byte statusLight;
+float valueDoAm, valueNhietDo = 0;
+int valueSOIL;
+
+void setup() {
+  pinMode(DHTPIN, INPUT);
+  pinMode(pinSOIL, INPUT);
+  pinMode(pinLight, INPUT);
+
+  dht.begin();
+
+  DebugSerial.begin(9600);
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+}
+
+void loop() {
+  Blynk.run();
+  valueSOIL = analogRead(pinSOIL); // CHECK - Độ ẩm đất
+  valueSOIL = map(pinSOIL, 0, 1023, 0, 100);
+  statusLight = digitalRead(pinLight); // CHECK - AS
+
+  valueDoAm = dht.readHumidity(); // CHECK - Độ ẩm không khí
+  valueNhietDo = dht.readTemperature(); // CHECK - Nhiệt độ
+
+  DebugSerial.print("Nhiet do: ");
+  DebugSerial.println(valueNhietDo);
+  DebugSerial.print("Do am KK: ");
+  DebugSerial.println(valueDoAm);
+  DebugSerial.print("Do am DAT: ");
+  DebugSerial.println(valueSOIL);
+  DebugSerial.print("Sang/Toi: ");
+  DebugSerial.println(statusLight);
+
+  Blynk.virtualWrite(V0, valueNhietDo); // Blynk - Nhiệt độ
+  Blynk.virtualWrite(V1, valueDoAm); // Blynk - Đ.A Không khí
+  Blynk.virtualWrite(V2, valueSOIL); // Blynk - Đ.A Đất
+  Blynk.virtualWrite(V3, statusLight); // ANH SANG
+  DebugSerial.println();
+}
