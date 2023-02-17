8018a2be2542f5739e49453929b40adead5d1d72
Hữu Thọ CS <89997693+huuthocs@users.noreply.github.com>
2023-02-17 12:52:47 +0700

    Update12h52pm17-02-23

--- Cay001/Cay001.ino
+++ Cay001/Cay001.ino
@@ -1,11 +1,15 @@
 #include <BlynkSimpleStream.h> // Blynk
 #include <DHT.h> // Thư viện DHT11
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
 
 #define DHTPIN A0
 #define pinSOIL A1
 
-#include <SoftwareSerial.h>
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4); // Khởi tạo chân giao tiếp
+
 SoftwareSerial DebugSerial(2, 3); // RX, TX
+SoftwareSerial SerialBluetooth(9, 10); // RX, TX
 
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
@@ -16,11 +20,46 @@ DHT dht(DHTPIN, DHTTYPE);
 #define BLYNK_AUTH_TOKEN "Tts2WJWwPD_a1c0OejzikcYrdszZwrKk"
 char auth[] = "Tts2WJWwPD_a1c0OejzikcYrdszZwrKk";
 
-byte statusLight;
+byte statusLight = 0;
 float valueDoAm, valueNhietDo = 0;
 int valueSOIL = 0;
+unsigned long Time = 0, timeDHT = 0;
+
+
+void PrintLCD(){
+  lcd.clear(); // Xóa dữ liệu LCD
+  lcd.setCursor(0,0); // Đặt con trỏ ở vị trí cột 0, hàng 0
+  lcd.print("T:");
+  lcd.print(valueNhietDo);
+  //lcd.print((char)223);
+  //lcd.print("C");
+
+  lcd.setCursor(9, 0);
+  lcd.print("SOIL:");
+  lcd.print(valueSOIL);
+  //lcd.print("%");
+
+  lcd.setCursor(0, 1);
+  lcd.print("DA:");
+  lcd.print(valueDoAm);
+  //lcd.print("%");
+}
+
+void PrintDebugSerial(){
+  DebugSerial.print("Nhiet do: ");
+  DebugSerial.println(valueNhietDo);
+  DebugSerial.print("Do am KK: ");
+  DebugSerial.println(valueDoAm);
+  DebugSerial.print("Do am DAT: ");
+  DebugSerial.println(valueSOIL);
+  DebugSerial.println();
+}
 
 void setup() {
+  SerialBluetooth.begin(9600);
+  lcd.begin(16, 2);
+  PrintLCD();
+
   pinMode(DHTPIN, INPUT);
   pinMode(pinSOIL, INPUT);
   // pinMode(pinLight, INPUT);
@@ -32,25 +71,43 @@ void setup() {
   Blynk.begin(Serial, auth);
 }
 
-void loop() {
-  Blynk.run();
+void LayThongSo(){
   valueSOIL = analogRead(pinSOIL); // CHECK - Độ ẩm đất
   valueSOIL = map(valueSOIL, 0, 1023, 0, 100);
-  // statusLight = digitalRead(pinLight); // CHECK - AS
+  // if(Time - timeDHT <= 1000){
+    valueDoAm = dht.readHumidity(); // CHECK - Độ ẩm không khí
+    valueNhietDo = dht.readTemperature(); // CHECK - Nhiệt độ
+    // valueDoAm = random(100);
+    // valueNhietDo = random(100);
+    // valueSOIL = random(100);
+    // timeDHT = Time;
+  // }
+}
 
-  valueDoAm = dht.readHumidity(); // CHECK - Độ ẩm không khí
-  valueNhietDo = dht.readTemperature(); // CHECK - Nhiệt độ
+void PullMaster(){
+  SerialBluetooth.print(valueDoAm);
+  SerialBluetooth.print(valueNhietDo);
+  SerialBluetooth.print(valueSOIL);
+}
 
-  DebugSerial.print("Nhiet do: ");
-  DebugSerial.println(valueNhietDo);
-  DebugSerial.print("Do am KK: ");
-  DebugSerial.println(valueDoAm);
-  DebugSerial.print("Do am DAT: ");
-  DebugSerial.println(valueSOIL);
-
+void PullBlynk(){
   Blynk.virtualWrite(V0, valueNhietDo); // Blynk - Nhiệt độ
   Blynk.virtualWrite(V1, valueDoAm); // Blynk - Đ.A Không khí
   Blynk.virtualWrite(V2, valueSOIL); // Blynk - Đ.A Đất
-  // Blynk.virtualWrite(V3, statusLight); // ANH SANG
-  DebugSerial.println();
+  Blynk.virtualWrite(V3, statusLight); // ANH SANG
+}
+
+void Xuly(){
+  // A, D o am duoi 40 gui dữ lieu
+  if(valueSOIL < 40) SerialBluetooth.write('A');
+}
+
+void loop() {
+  Time = millis();
+  Blynk.run();
+  LayThongSo();
+  Xuly();
+  PrintDebugSerial();
+  PrintLCD();
+  PullBlynk();
 }
--- TrungTam/TrungTam.ino
+++ TrungTam/TrungTam.ino
@@ -1,18 +1,22 @@
 // Import thư viện
 #include <LiquidCrystal.h> // Khai báo thư viện LCD / Kết nối qua SPI (Được hỗ trợ sẵn)
+#include "Wire.h"
+#include <RTClib.h> // Thư viện RTC
 #include <SPI.h>
 #include <Ethernet.h>
 #include <BlynkSimpleStream.h> // Blynk
+#include <SoftwareSerial.h>
 
 // RTC
-// RTC_DS1307 rtc;
+RTC_DS1307 rtc; // Khai báo biến
 // char daysOfTheWeek[7][12]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+// char daysOfTheWeek[7][12] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+char daysOfTheWeek[7][12] = {"ChN", "Hai", "Ba", "Tu", "Nam", "Sau", "Bay"};
 
 // Khởi tạo chân
 LiquidCrystal lcd(9, 8, 5, 4, 3, 2); // Khởi tạo chân giao tiếp
-
-#include <SoftwareSerial.h>
 SoftwareSerial DebugSerial(10, 11); // RX, TX
+SoftwareSerial SerialBluetooth(15, 14); // RX, TX
 
 // Blynk
 #define BLYNK_TEMPLATE_ID "TMPLSWy0uUgW"
@@ -22,43 +26,65 @@ char auth[] = "Tts2WJWwPD_a1c0OejzikcYrdszZwrKk";
 
 // Khai báo chân (Hằng số)
 #define pinNhietDo A0
-// #define pinLight 13
-#define pinRAIN 12
-#define motorPin 7
-// #define LAMpPin 6
+#define motor_bomnuoc 26
+#define pinEcho 25 // Chan echo cua HCSR04
+#define pinTrigg 24 // Chan trigger cua HCSR04
+#define pinBtnEdit 23
+#define pinBtnOK 22
+#define pinBtnDOWN 21
+#define pinBtnUP 20
+#define pinBtnLAMP 19
+#define pinBtnMotor 18
+#define pinBtnTime 17
+#define pinBtnAuto 16
+#define pinBtnReset 15
 #define pinLightVuon 14
+#define pinLDR 13
+#define pinRAIN 12
+#define pinMotor 7
+#define pinLAMP 6
+
 bool statusRain = 0;
 int statusLight, valueDoAmDat, giatriDAKK = 0;
 float giatriNhietDo;
-// DateTime now;
+byte nbMode = 0, statusEdit = 0, pageEdit = 0, nbModeLate = 0;
+DateTime now;
+char valuebth;
 
 void setup() {
   Serial.begin(9600);
+  DebugSerial.begin(9600);
+  SerialBluetooth.begin(9600);
 
-  // rtc.begin();
-  // if (!rtc.isrunning()){
-  //   Serial.println("RTC is NOT running");
-  //   rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
-  // }
+  if (!rtc.begin()){
+   DebugSerial.println("Couldn't find RTC");
+   while(1);
+  }
 
-  lcd.begin(16, 2); // Khởi tạo LCD 16, 2
-  // dht.begin();
+  if (!rtc.isrunning()){
+   DebugSerial.println("RTC is NOT running!");
+  }
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  lcd.begin(20, 4); // Khởi tạo LCD 16, 2
 
   // pinMode(pinLight, INPUT);
   pinMode(pinRAIN, INPUT);
-  pinMode(motorPin, OUTPUT);
-  // pinMode(LAMpPin, OUTPUT);
+  pinMode(pinMotor, OUTPUT);
+  pinMode(motor_bomnuoc, OUTPUT);
+  pinMode(pinLAMP, OUTPUT);
   pinMode(pinLightVuon, OUTPUT);
 
-  DebugSerial.begin(9600);
+  pinMode(pinTrigg, OUTPUT);
+  pinMode(pinEcho, INPUT);
+
+  
   Blynk.begin(Serial, auth);
 }
 
 BLYNK_CONNECTED() {
-    // Blynk.syncAll();
-    Blynk.syncVirtual(V0); // Phải dùng câu lệnh này
-    // Blynk.syncVirtual(V1);
-    Blynk.syncVirtual(V2); // Blynk - D.A_Dat
+    Blynk.syncAll();
+    Blynk.syncVirtual(V0, V1, V3); // Phải dùng câu lệnh này, V3>>D.A_Dat
 }
 
 BLYNK_WRITE(V0){
@@ -73,28 +99,19 @@ BLYNK_WRITE(V2){
   valueDoAmDat = param.asInt();
 }
 
+BLYNK_WRITE(V10){
+  boolean value = param.asInt();
+  digitalWrite(pinMotor, value);
+}
+
 BLYNK_WRITE(V20){
   int pin = param.asInt();
   digitalWrite(pinLightVuon, pin);
 }
 
-// void ModuleRTC(){
-//   // LAY THOI GIAN TU RTC
-//   now=rtc.now();
-//   DebugSerial.print(now.year(), DEC);
-//   DebugSerial.print(" ");
-//   DebugSerial.print(now.month(), DEC);
-//   DebugSerial.print(" ");
-//   DebugSerial.print(now.day(), DEC);
-//   DebugSerial.print(" ");
-//   DebugSerial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-//   DebugSerial.print(" ");
-//   DebugSerial.print(now.hour(), DEC);
-//   DebugSerial.print(" ");
-//   DebugSerial.print(now.minute(), DEC);
-//   DebugSerial.print(" ");
-//   DebugSerial.println(now.second(), DEC);
-// }
+void ModuleRTC(){ // LAY THOI GIAN TU RTC
+  now=rtc.now();
+}
 
 void PrintSerial(){
   DebugSerial.print("Nhiet do: ");
@@ -112,78 +129,163 @@ void PrintSerial(){
     DebugSerial.println("Mua.");
   else
     DebugSerial.println("Nang.");
+  DebugSerial.println();
+}
+
+void PrintLCD(){
+  lcd.clear(); // Xóa dữ liệu LCD
+  lcd.setCursor(0,1); // Đặt con trỏ ở vị trí cột 0, hàng 0
+  lcd.print("Mode: ");
+  switch(nbMode){
+    case 0:
+      lcd.print("Cust");
+      break;
+    case 1:
+      lcd.print("Auto");
+      break;
+    case 2:
+      lcd.print("Time");
+      break;
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  lcd.print(", ");
+  lcd.print(now.day(), DEC);
+  lcd.print("/");
+  lcd.print(now.month(), DEC);
+  lcd.print(" ");
+  lcd.print(now.hour(), DEC);
+  lcd.print(":");
+  lcd.print(now.minute(), DEC);
+  lcd.print(":");
+  lcd.println(now.second(), DEC);
 }
 
 void sensorRAIN(){
   statusRain = digitalRead(pinRAIN);
 }
 
-// void PrintLCD(){
-//   lcd.clear(); // Xóa dữ liệu LCD
-//   lcd.setCursor(0,0); // Đặt con trỏ ở vị trí cột 0, hàng 0
-//   lcd.print("T:");
-//   lcd.print(giatriNhietDo);
+void sensorLight(){
+  statusLight = digitalRead(pinLDR);
+}
 
-//   lcd.setCursor(8, 0);
-//   lcd.print("SOIL:");
-//   lcd.print(valueDoAmDat);
+void Next(){
+  PrintLCD();
+  PrintSerial();
+}
 
-//   lcd.setCursor(0, 1);
-//   lcd.print("DA:");
-//   lcd.print(giatriDAKK);
-//   lcd.print("%");
+void DoKhC(){
+  float duration; // bien do thoi gian
+  float distance; // bien luu khoang cach
+  digitalWrite(pinTrigg, 0); // tat chan trigg
+  delayMicroseconds(2);
+  digitalWrite(pinTrigg, 1); // phat xung tu chan trig
+  delayMicroseconds(10);
+  digitalWrite(pinTrigg, 1);
+  duration = pulseIn(pinEcho, 1, 30000); // do rong xung HIGH o chan echo sau 30000us khong nhan duoc xung phan xa thi tra ve 0
+  distance = (duration*0.034/2);
+  DebugSerial.print("Khoan Cach: ");
+  DebugSerial.print(distance);
+  DebugSerial.println("cm ");
+}
 
-//   lcd.setCursor(10, 1);
-//   lcd.print("AS:");
-//   lcd.print(statusLight);
-// }
+//--- RUN THIET BI
+void RunMotor(){
+  // Run -motor
+  if(valueDoAmDat <= 45){
+    sensorRAIN();
+    if (statusRain == 0){
+      Blynk.virtualWrite(V10, 1);
+      digitalWrite(pinMotor, HIGH);
+    } else {
+      Blynk.virtualWrite(V10, 0);
+      digitalWrite(pinMotor, LOW);
+    }
+  } else {
+      Blynk.virtualWrite(V10, 0);
+      digitalWrite(pinMotor, LOW);
+  }
+}
 
-void ModeCust(){
+void runLAMP(){
+  // Run -LAMP
+  if(statusLight == 0)
+    digitalWrite(pinLAMP, 1);
+  else 
+    digitalWrite(pinLAMP, 0);
+}
+
+//-- MODE
+void CheckMode(){ //--Ok
+  // DebugSerial.print("Mode: ");
+  // DebugSerial.println(nbMode);
+  if(digitalRead(pinBtnReset)){
+    nbMode = 0;
+  } else if(digitalRead(pinBtnAuto)){
+    nbMode = 1;
+  } else if(digitalRead(pinBtnTime)){
+    nbMode = 2;
+  } 
+}
+
+void CustMode(){ // --Ok
+  if(digitalRead(pinBtnMotor)){
+    digitalWrite(pinMotor, !digitalRead(pinMotor));
+  } 
+  if(digitalRead(pinBtnLAMP)){
+    digitalWrite(pinLAMP, !digitalRead(pinLAMP));
+  } 
 
 }
 
-void ModeAuto(){
+void AutoMode(){
+  RunMotor();
+  runLAMP();
+}
 
+void TimeMode(){
+  RunMotor();
+  runLAMP();
+  // CAI DAT GIO TUOI CAY HANG NGAY (BUỔI SÁNG VÀ TỐI)
+  if(//(now.hour()>=12 && now.hour()<13)&&(now.minute()>=22&&now.minute()<=23)
+    ((now.hour()>=7 && now.hour()<8)&&(now.minute()>=30&&now.minute()<=50))
+                      ||
+    ((now.hour()>=17 && now.hour()<18)&&(now.minute()>=30&&now.minute()<=50))
+    ){
+      RunMotor();
+  }
 }
 
 void loop() {
-  Blynk.run();
-  sensorRAIN();
-  PrintSerial();
-
-  // ModuleRTC();
-  // DoNhietdoDoAm();
-  // sersorLight();
-  // DoDoAmDat();
-  // sersorRain();
-  // PrintLCD();
-
-  // //CAI DAT GIO TUOI CAY HANG NGAY (BUỔI SÁNG VÀ TỐI)
-  // if(((now.hour()>=7 && now.hour()<8)&&(now.minute()>=30&&now.minute()<=50))
-  //                     ||
-  //   ((now.hour()>=17 && now.hour()<18)&&(now.minute()>=30&&now.minute()<=50))){
-  //   digitalWrite(motorPin,HIGH);
-  // }
-  // else
-  //   digitalWrite(motorPin,LOW);
-
-  if(valueDoAmDat <= 45)
-    if (statusRain == 0){
-      Blynk.virtualWrite(V10, 1);
-      digitalWrite(motorPin, HIGH);
-    } else {
-      Blynk.virtualWrite(V10, 0);
-      digitalWrite(motorPin, LOW);
+  if(SerialBluetooth.available() > 0){
+    valuebth = Serial.read();
+    DebugSerial.print("vle_bth: ");
+    DebugSerial.println(valuebth);
+    if(valuebth == 'A'){
+        RunMotor();
     }
-  else {
-    Blynk.virtualWrite(V10, 0);
-    digitalWrite(motorPin, LOW);
   }
-  
-  // if(statusLight == 0)
-  //   digitalWrite(LAMpPin, HIGH);
-  // else 
-  //   digitalWrite(LAMpPin, LOW);
-  DebugSerial.println();
-  
+  Blynk.run();
+  ModuleRTC(); // Lấy dữ liệu giờ hiện tại.
+  // DoKhC();
+  // if(digitalRead(pinBtnEdit)){
+  //   nbModeLate = nbMode; 
+  //   nbMode = 3;
+  // }
+  CheckMode();
+  sensorLight();
+  PrintLCD();
+  // PrintSerial();
+  switch(nbMode){
+    case 0: // Thủ công
+      CustMode();
+      break;
+    case 1: // Tự động
+      AutoMode();
+      break;
+    case 2: // Hẹn giờ
+      TimeMode();
+      break;
+  }
 }
